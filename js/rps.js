d1a7890a617de1a399458c7b8acd0cd558e157b0
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 00:51:54 -0300

    Add ask user choice function

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,18 @@
 const CHOICES = ['rock','paper','scissors'];
+let usrChoice;
+
+function askUserChoice(){
+    let userInput = prompt("Please choose", "Rock/Paper/Scissors").toLowerCase().trim(); //Ask user's choice
+    if(CHOICES.includes(userInput)){                                                     //then convert to lowercase
+        console.log(`User selected ${userInput} !`);                                     //then remove whitespace
+        return userInput;
+    }
+    else{
+        askUserChoice(); //If input a value other than "ROCK/PAPER/SCISSORS" runs the function again
+    }
+}
 
 function computerPlay(){
-    return CHOICES[parseInt(Math.random() * 3)];
+    return CHOICES[parseInt(Math.random() * 3)]; //Chooses a random index between 0-2 
 }
 
